bd9ee65eed166270e18b51c6c0952ec480c85fb0
woodinnis <wood.innis@gmail.com>
2016-10-06 11:20:35 -0700

    Arrow tile functionality
    Created basic arrow tiles and implemented primary functions
    Arrow tiles trigger a change of playerDirection

---
+++ MayanMaze/Assets/Scripts/Arrow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour {
+
+    Player player;
+
+	// Use this for initialization
+	void Start () {
+        //  Get the player information
+        player = GameObject.FindObjectOfType<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D() {
+        switch (tag)
+        {
+            case "DownArrow":
+                player.playerDirection = Player.Direction.DOWN;
+                break;
+            case "UpArrow":
+                player.playerDirection = Player.Direction.UP;
+                break;
+            case "LeftArrow":
+                player.playerDirection = Player.Direction.LEFT;
+                break;
+            case "RightArrow":
+                player.playerDirection = Player.Direction.RIGHT;
+                break;
+        }
+    }
+}
