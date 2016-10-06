86d9342a2c7485076abb84aede9d0dde700c7bb2
woodinnis <wood.innis@gmail.com>
2016-10-06 14:33:45 -0700

    Door added
    Added a door, with basic functionality to progress to the next level in
    the Unity Scene list.

---
+++ MayanMaze/Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    private Player player;
+    private Transform transform;
+    //private int sceneBuildIndex;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindObjectOfType<Player>();
+        transform = GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void OnTriggerEnter2D()
+    {
+        print("DOOR!");
+        //  Reset player position to match the Arrow tile.
+        player.transform.position = transform.position;     //  This needs to be smoothed out during polish. But it works for now
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
