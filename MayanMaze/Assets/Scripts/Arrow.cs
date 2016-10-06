44a7772da9d79a6a2bf98963a21964b6bf36dc6e
woodinnis <wood.innis@gmail.com>
2016-10-06 12:04:58 -0700

    Arrow tiles movement
    Implemented the ability to move arrow tiles around the gamespace.
    Pulled functions from the Glitch Garden Unity tutorial for both
    mouse-to-worldspace, and snap-to-grid functionality.

--- MayanMaze/Assets/Scripts/Arrow.cs
+++ MayanMaze/Assets/Scripts/Arrow.cs
@@ -3,20 +3,21 @@ using System.Collections;
 
 public class Arrow : MonoBehaviour {
 
-    Player player;
+    private Player player;
+    private Transform transform;
+    private Camera myCamera;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         //  Get the player information
         player = GameObject.FindObjectOfType<Player>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+        myCamera = GameObject.FindObjectOfType<Camera>();
+
+        transform = GetComponent<Transform>();
 	}
 
     void OnTriggerEnter2D() {
+        //  Change player direction based on the type of (tag) arrow
         switch (tag)
         {
             case "DownArrow":
@@ -32,5 +33,33 @@ public class Arrow : MonoBehaviour {
                 player.playerDirection = Player.Direction.RIGHT;
                 break;
         }
+    }
+
+    void OnMouseDrag() {
+        //  Move the arrow around the gamespace by clicking and dragging
+        Vector2 rawPos = CalculateWorldPointOfMouseClick();
+        transform.position = SnapToGrid(rawPos);
+    }
+
+    Vector2 CalculateWorldPointOfMouseClick()
+    {
+        //  Calculate the worldspace of the mouse (instead of the default pixels used by Input.mousePosition)
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        float distanceFromCamera = 10f;
+
+        Vector3 weirdTriplet = new Vector3(mouseX, mouseY, distanceFromCamera);
+        Vector2 worldPos = myCamera.ScreenToWorldPoint(weirdTriplet);
+
+        return worldPos;
+    }
+
+    Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        //  Snap tiles to the gamespace grid
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+
+        return new Vector2(newX, newY);
     }
 }
