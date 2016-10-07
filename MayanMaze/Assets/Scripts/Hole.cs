7b40b82b34a9fc9f76dacd52c33f0d15460d7d71
woodinnis <wood.innis@gmail.com>
2016-10-07 11:10:12 -0700

    Added Holes
    Hole traps added
    Holes call LevelManager.resetLevel() onTriggerEnter2D
    May move this functionality to the hole itself in future.

---
+++ MayanMaze/Assets/Scripts/Hole.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Hole : MonoBehaviour {
+
+    private Player player;
+    private Transform transform;
+    private LevelManager levelManager;
+
+    private Scene scene;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindObjectOfType<Player>();
+        transform = GetComponent<Transform>();
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+    //  Reset level when player collides with the hole
+	void OnTriggerEnter2D(Collider2D collider) {
+        //  Snap player to the position of the hole
+        player.transform.position = transform.position;
+
+        //  Reload the level
+        levelManager.RestartLevel();
+    }
+}
--- MayanMaze/Assets/Scripts/LevelManager.cs
+++ MayanMaze/Assets/Scripts/LevelManager.cs
@@ -7,7 +7,8 @@ public class LevelManager : MonoBehaviour {
     public float autoLoadNextLevelAfter;
 
     void Start() {
-        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        
+        //Invoke("LoadNextLevel", autoLoadNextLevelAfter);
     }
 	public void LoadLevel(string name){
         SceneManager.LoadScene(name);
@@ -19,5 +20,20 @@ public class LevelManager : MonoBehaviour {
 	
 	public void LoadNextLevel(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //  Restart a level
+    public void RestartLevel()
+    {
+        //  Get currently loaded scene;
+        //Scene scene = SceneManager.GetActiveScene();
+
+        //print(scene.ToString());
+        //  Unload the scene
+        //SceneManager.UnloadScene(scene);
+
+        //  Reload the scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
     }
 }
