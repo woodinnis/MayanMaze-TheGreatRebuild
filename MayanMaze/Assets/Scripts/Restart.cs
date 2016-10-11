6dda121b210b39d9913cdaa1c8c472a23ad3b124
woodinnis <wood.innis@gmail.com>
2016-10-11 15:19:50 -0700

    Rebuilt levels as squares
    Rebuilt all test levels as 8x8 squares
    Rebuilt Wall prefabs
    Repositioned all scene elements
    This leaves a significant amount of space for UI elements on the
    right-hand side of the screen.

---
+++ MayanMaze/Assets/Scripts/Restart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Restart : MonoBehaviour {
+
+    private LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	void OnMouseDown()
+    {
+        levelManager.RestartLevel();
+    }
+}
