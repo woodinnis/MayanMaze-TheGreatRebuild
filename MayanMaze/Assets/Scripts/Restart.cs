ac3ef2be5fff045ef009da4d153ffcf3b72a23e8
woodinnis <wood.innis@gmail.com>
2016-10-11 16:18:25 -0700

    Reset Buttion moved to UI
    The reset button has been moved to the UI layer
    Created a new UI Button
    Rewrote Restart.cs to work with a UI element
    Rebuilt the Level Canvas Prefab
    Tested on all current levels

--- MayanMaze/Assets/Scripts/Restart.cs
+++ MayanMaze/Assets/Scripts/Restart.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Restart : MonoBehaviour {
 
+    private Button button;
     private LevelManager levelManager;
 
 	// Use this for initialization
 	void Start () {
+
+        button = GetComponent<Button>();
+
         levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+        button.onClick.AddListener(TaskOnClick);
 	}
 	
-	void OnMouseDown()
+    void TaskOnClick()
     {
         levelManager.RestartLevel();
     }
