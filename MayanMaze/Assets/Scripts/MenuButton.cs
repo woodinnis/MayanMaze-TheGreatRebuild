6909962a1f738dba322177ce9485d7657870ae15
woodinnis <wood.innis@gmail.com>
2016-10-17 15:24:19 -0700

    Added "Menu" Button to UI
    New button on UI Canvas allows players to return to the menu from any
    level
    
    MenuButton.cs uses almost identical code to Restart.cs, calls
    LevelManager.GoToStart() instead. Should probably be optimised somehow.

---
+++ MayanMaze/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour {
+
+    private Button button;
+    private LevelManager levelManager;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        button = GetComponent<Button>();
+
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        levelManager.GoToStart();
+    }
+}
