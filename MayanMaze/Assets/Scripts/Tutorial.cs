61593bb19618541d14b8be4f1b126a801acc3be1
woodinnis <wood.innis@gmail.com>
2016-11-07 10:29:42 -0800

    On screen tutorial fixes
    On screen tutorial can now be dismissed by clicking the "Continue"
    button.
    - Added a new Tutorial Canvas beneath the main UI Canvas
    - Added a Tutorial Panel to the Tutoral Canvas
    - All Tutorial Messages are now part of the Tutoral Panel, instead of
    the main game UI
    - Added a Continue button to the Tutorial Canvas
    - Tutorial.cs now verifies the current level is present in
    tutorialMessages[] and disables TutoralPanel if it is not
    - TutorialButton.cs TaskOnClick() disables TutorialCanvas

--- MayanMaze/Assets/Scripts/Tutorial.cs
+++ MayanMaze/Assets/Scripts/Tutorial.cs
@@ -5,16 +5,18 @@ using System.Collections;
 public class Tutorial : MonoBehaviour {
 
     public string[] checkLevels;
-    public string[] tutorialMessages;
+    public Text[] tutorialMessages;
 
     private LevelManager levelManager;
     private Text text;
+    private Canvas tutorialCanvas;
 
 	// Use this for initialization
 	void Start ()
     {
         levelManager = GameObject.FindObjectOfType<LevelManager>();
         text = GetComponent<Text>();
+        tutorialCanvas = GetComponentInParent<Canvas>();
 
         DisplayCurrentTutorial();
     }
@@ -31,10 +33,14 @@ public class Tutorial : MonoBehaviour {
             //  When a matching level name and tutorial message are found, display the message.
             if (level == currentLevel)
             {
-                text.text = tutorialMessages[index];
+                text.text = tutorialMessages[index].text;
                 break;
             }
             ++index;
+        }
+        if(index > tutorialMessages.Length)
+        {
+            tutorialCanvas.enabled = false;
         }
     }
 }
---
+++ MayanMaze/Assets/Scripts/TutorialButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutorialButton : MonoBehaviour {
+
+    private GameObject tutorialPanel;
+    private Button button;
+    private Canvas tutorialCanvas;
+
+    // Use this for initialization
+    void Start () {
+        tutorialPanel = GameObject.Find("Tutorial Panel");
+        button = GetComponent<Button>();
+        tutorialCanvas = GetComponentInParent<Canvas>();
+
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        tutorialCanvas.enabled = false;
+    }
+}
