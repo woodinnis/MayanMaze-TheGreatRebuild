6526e0328e2ebd5cb93ab4d7e2425438f0831160
woodinnis <wood.innis@gmail.com>
2016-11-16 16:40:10 -0800

    Pause Button Added
    Pause button added to Pause Screen prefab
    - Pause button un/pauses game
    -- Calls PauseScreen.cs PauseFunction()
    - PauseScreen.cs added ButtonHandler()
    -- Sets visibility of Menu and Reset buttons

---
+++ MayanMaze/Assets/Scripts/PauseButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PauseButton : MonoBehaviour {
+
+    private Button button;
+    private PauseScreen pauseScreen;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        button = GetComponent<Button>();
+        pauseScreen = GetComponentInParent<PauseScreen>();
+
+        button.onClick.AddListener(TaskOnClick);
+
+    }
+
+    void TaskOnClick()
+    {
+        pauseScreen.PauseFunction();
+    }
+}
--- MayanMaze/Assets/Scripts/PauseScreen.cs
+++ MayanMaze/Assets/Scripts/PauseScreen.cs
@@ -9,6 +9,9 @@ public class PauseScreen : MonoBehaviour {
     private MenuButton menuButton;
     private RestartButton restartButton;
 
+    //  I Cannot currently make a panel work the way I'd like to, so I'm leaving the Canvas out for now.
+    //    private Canvas pauseCanvas;
+
     private Arrow[] arrows;
     private Player player;
 
@@ -21,46 +24,74 @@ public class PauseScreen : MonoBehaviour {
         pauseButton = GetComponentInChildren<PauseButton>();
         menuButton = GetComponentInChildren<MenuButton>();
         restartButton = GetComponentInChildren<RestartButton>();
+        //  pauseCanvas = GetComponent<Canvas>();
 
         arrows = FindObjectsOfType<Arrow>();
         player = FindObjectOfType<Player>();
 
         gameIsPaused = false;
+        ButtonHandler();
+
+        //pauseCanvas.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update() {
         if (Input.GetKeyDown("space"))
+        {
             PauseFunction();
+        }
 	}
 
     //  The pause function disables primary game elements
-    void PauseFunction()
+    public void PauseFunction()
     {
 
         if (!gameIsPaused)
         {
+            //pauseCanvas.enabled = true;
             player.enabled = false;
             player.GetComponent<SpriteRenderer>().enabled = false;
-            pauseText.text = "This is not a potato";
+            pauseText.text = "PAUSED";
             foreach (Arrow tile in arrows)
             {
                 tile.enabled = false;
                 tile.GetComponent<SpriteRenderer>().enabled = false;
             }
             gameIsPaused = true;
+            ButtonHandler();
         }
         else
         {
+            //pauseCanvas.enabled = false;
             player.enabled = true;
             player.GetComponent<SpriteRenderer>().enabled = true;
-            pauseText.text = "You May Play Now";
+            pauseText.text = "";
             foreach (Arrow tile in arrows)
             {
                 tile.enabled = true;
                 tile.GetComponent<SpriteRenderer>().enabled = true;
             }
             gameIsPaused = false;
+            ButtonHandler();
+        }
+    }
+
+    void ButtonHandler()
+    {
+        if (!gameIsPaused)
+        {
+            restartButton.enabled = false;
+            restartButton.GetComponent<Image>().enabled = false;
+            menuButton.enabled = false;
+            menuButton.GetComponent<Image>().enabled = false;
+        }
+        else
+        {
+            restartButton.enabled = true;
+            restartButton.GetComponent<Image>().enabled = true;
+            menuButton.enabled = true;
+            menuButton.GetComponent<Image>().enabled = true;
         }
     }
 }
