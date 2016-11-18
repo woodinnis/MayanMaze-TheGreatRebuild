3583e737a026849a058d38eaa1d464ff008bbce1
woodinnis <wood.innis@gmail.com>
2016-11-17 16:04:19 -0800

    Graphic and Countdown Adjustment
    Adjusted the Pause Screen font colour, and added a shadow to match the
    the Countdown Timer
    
    Pause button is now disabled during Countdown

--- MayanMaze/Assets/Scripts/CountdownTimer.cs
+++ MayanMaze/Assets/Scripts/CountdownTimer.cs
@@ -12,11 +12,10 @@ public class CountdownTimer : MonoBehaviour {
     private Text timerText;
     private Player currentPlayer;
     private float countdownTarget;
+    private PauseButton pauseButton;
 
 	// Use this for initialization
 	void Start () {
-
-        print(startAtSceneIndex);
 
 	}
 	
@@ -36,18 +35,31 @@ public class CountdownTimer : MonoBehaviour {
             {
                 currentPlayer.enabled = true;
                 timerText.enabled = false;
+
+                //  Enable pause button
+                pauseButton.enabled = true;
             }
         }
     }
 
     void OnLevelWasLoaded()
     {
+        //  Varify that the current scene is at or above the first scene requiring a timer
         if (SceneManager.GetActiveScene().buildIndex >= startAtSceneIndex)
         {
+            //  Set the local countdown timer value
             countdownTarget = countdownTime;
-            timerText = GetComponentInChildren<Text>();
+
+            //  Disable the pause button
+            pauseButton = FindObjectOfType<PauseButton>();
+            pauseButton.enabled = false;
+
+            //  Disable the player
             currentPlayer = FindObjectOfType<Player>();
             currentPlayer.enabled = false;
+
+            //  Enable the timer text
+            timerText = GetComponentInChildren<Text>();
             timerText.enabled = true;
         }
 
