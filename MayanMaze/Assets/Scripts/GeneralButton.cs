d1e44ad743cc948d613571ed02af3afa2b643a6e
woodinnis <wood.innis@gmail.com>
2016-11-22 11:55:27 -0800

    UI optimization - Buttons

---
+++ MayanMaze/Assets/Scripts/GeneralButton.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GeneralButton : MonoBehaviour {
+
+    public enum ButtonType { MENU, PAUSE, RESTART, TUTORIAL };
+
+    public ButtonType buttonType;
+
+    private Button button;
+    private Canvas tutorialCanvas;
+    private GameObject tutorialPanel;
+    private PauseScreen pauseScreen;
+
+    // Use this for initialization
+    void Start() {
+        
+        button = GetComponent<Button>();
+
+        //tutorialCanvas = GetComponentInParent<Canvas>();
+        //tutorialPanel = GameObject.Find("Tutorial Panel");
+        //pauseScreen = FindObjectOfType<PauseScreen>();
+
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        switch (buttonType)
+        {
+            case ButtonType.MENU:
+                {
+                    LevelManager levelManager = FindObjectOfType<LevelManager>();
+                    levelManager.GoToStart();
+                    print("Menu");
+                    break;
+                }
+            case ButtonType.PAUSE:
+                {
+                    print("Pause");
+                    break;
+                }
+            case ButtonType.RESTART:
+                {
+                    LevelManager levelManager = FindObjectOfType<LevelManager>();
+                    levelManager.RestartLevel();
+                    print("Restart");
+                    break;
+                }
+            case ButtonType.TUTORIAL:
+                {
+                    print("Tutorial");
+                    break;
+                }
+        }
+        //tutorialCanvas.enabled = false;
+        //pauseScreen.PauseFunction();
+    }
+
+}
