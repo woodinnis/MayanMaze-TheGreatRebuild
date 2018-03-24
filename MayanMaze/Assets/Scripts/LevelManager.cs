2226b2146fa6749d28885f561098976d4370d9a9
wood_innis <wood.innis@gmail.com>
2018-03-23 17:26:50 -0700

    Rebuilding Level Manager
    Some reorganization of the code layout into blocks
    
    LoadNextLevel now uses the Levels list

--- MayanMaze/Assets/Scripts/LevelManager.cs
+++ MayanMaze/Assets/Scripts/LevelManager.cs
@@ -24,11 +24,14 @@ public class LevelManager : MonoBehaviour {
     
     void Start() {
 
+        
         // Load up the Levels List with all enabled scenes.
         foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
         {
-            if(scene.enabled)
+            if (scene.enabled)
+            {
                 Levels.Add(scene.path.ToString());
+            }
         }
 
         Invoke("LoadStartMenu", autoLoadNextLevelAfter);
@@ -39,52 +42,15 @@ public class LevelManager : MonoBehaviour {
         SceneManager.LoadScene(startScene);
     }
 
+    /// <summary>
+    /// All functions in the GetCurrentFunctions region are used to obtain and return current level information
+    /// </summary>
+    /// <returns></returns>
+    #region GetCurrentFunctions
+
     public int GetCurrentLevelIndex()
     {
         return SceneManager.GetActiveScene().buildIndex;
-    }
-
-    int GetNextLevelIndex()
-    {
-
-        int nextLevelIndex = 0;
-        int currentLevelIndex = GetCurrentLevelNumber();
-
-//        Debug.Log("Current Level Index: " + currentLevelIndex);
-
-        for (int i = currentLevelIndex; i < levelList.Length; i++)
-        {
-            //Debug.Log(levelList[i]);
-            
-            // This section correctly loads the names of the next levelList entry but still isn't checking if the name is valid in the build settings
-            if (SceneManager.GetSceneByName(levelList[i]).IsValid())
-            {
-                nextLevelIndex = i + 1;
-                //Debug.Log("Next Valid Scene " + levelList[nextLevelIndex] + " at index " + nextLevelIndex);
-                break;
-            }
-            else
-            {
-               // Debug.Log("No Valid Scene at index " + i);
-            }
-        }
-
-        //if (currentLevelName != levelList[currentBuildIndex])
-        //{
-        //    Debug.Log("Invalid Level " + currentLevelName + " at Index " + currentBuildIndex);
-        //    for (int i = currentBuildIndex; i < levelList.Length; i++)
-        //    {
-        //        if (levelList[i] == currentLevelName)
-        //        {
-        //            currentBuildIndex = i;
-        //            Debug.Log("Level Index Corrected");
-        //        }
-        //    }
-        //}
-        //else
-        //    Debug.Log("Current Level " + currentLevelName + " at Index " + currentBuildIndex);
-
-        return nextLevelIndex;
     }
 
     public string GetCurrentLevelName()
@@ -94,8 +60,6 @@ public class LevelManager : MonoBehaviour {
         return currentLevelName;
     }
 
-    // Returns the LevelList array index of the currently loaded level
-    // Checks the Level Name against all entries in the LevelList using a for loop and returns the valid array index
     public int GetCurrentLevelNumber()
     {
         string currentLevelName = GetCurrentLevelName();
@@ -112,18 +76,48 @@ public class LevelManager : MonoBehaviour {
         return currentLevelIndex;
     }
 
-    // Load the next level in the Level List array, 
-    // Checks the currently loaded level against finalScene and loads the next LevelList entry until the check returns true
-    //
-    // An exception is required for levels included in the LevelList, but not in the Build.
-    // SceneManager.LoadScene() uses Build Index or Scene Names
-	public void LoadNextLevel(){
+    int GetNextLevelIndex()
+    {
+        int currentLevelIndex = GetCurrentLevelNumber();
+        int nextLevelIndex = 0;
+
+        if (currentLevelIndex + 1 < Levels.Capacity)
+            nextLevelIndex = currentLevelIndex + 1;
+
+        return nextLevelIndex;
+        //Debug.Log("Current Level Index: " + currentLevelIndex);
+
+        //for (int i = currentLevelIndex; i < levelList.Length; i++)
+        //{
+        //    //Debug.Log(levelList[i]);
+
+        //    // This section correctly loads the names of the next levelList entry but still isn't checking if the name is valid in the build settings
+        //    if (SceneManager.GetSceneByName(levelList[i]).IsValid())
+        //    {
+        //        nextLevelIndex = i + 1;
+        //        //Debug.Log("Next Valid Scene " + levelList[nextLevelIndex] + " at index " + nextLevelIndex);
+        //        break;
+        //    }
+        //    else
+        //    {
+        //       // Debug.Log("No Valid Scene at index " + i);
+        //    }
+        //}
+    }
+
+    #endregion
+
+    public void LoadNextLevel(){
 
         if (GetCurrentLevelName() != finalScene)
         {
             int nLevel = GetNextLevelIndex();
 
-            SceneManager.LoadScene(levelList[nLevel]);//GetCurrentLevelNumber() + 1);
+            // I'd like to find a more efficient way to clean up the strings. Preferably when they get loaded into the list.
+            string nextScene = Levels[nLevel];
+            nextScene = nextScene.Replace(".unity", "");
+            nextScene = nextScene.Replace("Assets/", "");
+            SceneManager.LoadScene(nextScene);
         }
         else
             LoadStartMenu();
@@ -135,5 +129,5 @@ public class LevelManager : MonoBehaviour {
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-
+ 
 }
