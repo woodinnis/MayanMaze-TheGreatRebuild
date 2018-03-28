c0e302e29c0145cb39b8db1db6ce6ab2eeabf295
wood_innis <wood.innis@gmail.com>
2018-03-28 11:52:10 -0700

    Building a Game State Manager
    First commit to branch

---
+++ MayanMaze/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateManager : MonoBehaviour {
+
+    // Keep the GameStateManager in all subsequent scenes
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
